5b91d24ce532bef001b3bbaaab2a8dcccf65ac10
aaronzygala <azygala2016@fau.edu>
2020-03-22 17:16:34 -0400

    quick fix

--- motion.py
+++ motion.py
@@ -16,42 +16,9 @@ class Motion:
         inputArray = inputVal
         return inputArray
 
-    def isVelWithinRange(self, gyroValCompare, gyroValMovement):
-        minRange = 0;
-        maxRange = 0;
-        if (gyroValCompare > 0):
-            if (gyroValCompare > 1000):
-                maxRange = gyroValCompare + 500
-                minRange = gyroValCompare - 500
-            elif (gyroValCompare <= 1000 and gyroValCompare > 500):
-                maxRange = gyroValCompare + 100
-                minRange = gyroValCompare - 100
-            elif (gyroValCompare <= 500 and gyroValCompare > 100):
-                maxRange = gyroValCompare + 50
-                minRange = gyroValCompare - 50
-            elif (gyroValCompare <= 100):
-                maxRange = gyroValCompare + 10
-                minRange = gyroValCompare - 10
-        else:
-            if (gyroValCompare < -1000):
-                minRange = gyroValCompare + 500
-                maxRange = gyroValCompare - 500
-            elif (gyroValCompare >= -1000 and gyroValCompare < -500):
-                minRange = gyroValCompare + 100
-                maxRange = gyroValCompare - 100
-            elif (gyroValCompare >= -500 and gyroValCompare < -100):
-                minRange = gyroValCompare + 50
-                maxRange = gyroValCompare - 50
-            elif (gyroValCompare >= -100):
-                minRange = gyroValCompare + 10
-                maxRange = gyroValCompare - 10
 
-        return (gyroValMovement - minRange) * (gyroValMovement - maxRange) <= 0
 
-    def isAngleWithinRange(self, accelValA, accelValB):
-        minRange = accelValA * 100 - 5
-        maxRange = accelValA * 100 + 5
-        return (accelValB * 100 - minRange) * (accelValB * 100 - maxRange) <= 0
+
 
 
     def compare_motions(self, motion1, motion2):
@@ -60,6 +27,43 @@ class Motion:
             return true
         else:
             return false
+
+
+def isAngleWithinRange(accelValA, accelValB):
+    minRange = accelValA * 100 - 5
+    maxRange = accelValA * 100 + 5
+    return (accelValB * 100 - minRange) * (accelValB * 100 - maxRange) <= 0
+def isVelWithinRange(gyroValCompare, gyroValMovement):
+    minRange = 0;
+    maxRange = 0;
+    if (gyroValCompare > 0):
+        if (gyroValCompare > 1000):
+            maxRange = gyroValCompare + 500
+            minRange = gyroValCompare - 500
+        elif (gyroValCompare <= 1000 and gyroValCompare > 500):
+            maxRange = gyroValCompare + 100
+            minRange = gyroValCompare - 100
+        elif (gyroValCompare <= 500 and gyroValCompare > 100):
+            maxRange = gyroValCompare + 50
+            minRange = gyroValCompare - 50
+        elif (gyroValCompare <= 100):
+            maxRange = gyroValCompare + 10
+            minRange = gyroValCompare - 10
+    else:
+        if (gyroValCompare < -1000):
+            minRange = gyroValCompare + 500
+            maxRange = gyroValCompare - 500
+        elif (gyroValCompare >= -1000 and gyroValCompare < -500):
+            minRange = gyroValCompare + 100
+            maxRange = gyroValCompare - 100
+        elif (gyroValCompare >= -500 and gyroValCompare < -100):
+            minRange = gyroValCompare + 50
+            maxRange = gyroValCompare - 50
+        elif (gyroValCompare >= -100):
+            minRange = gyroValCompare + 10
+            maxRange = gyroValCompare - 10
+
+    return (gyroValMovement - minRange) * (gyroValMovement - maxRange) <= 0
 
 
 
