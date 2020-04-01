fbb33184d9c6d6f93ddeacd84110c21f7eff3be3
aaronzygala <azygala2016@fau.edu>
2020-04-01 17:55:07 -0400

    updated input class

--- input.py
+++ input.py
@@ -8,9 +8,6 @@ class Input:
     def __init__(self, name):
         self.inputName = name
         self.movementVal = []
-        self.accelRangeX = 0
-        self.accelRangeY = 0
-        self.accelRangeZ = 0
         self.accelRanges = [self.accelRangeX, self.accelRangeY, self.accelRangeZ]
 
     def set_ranges(self):
