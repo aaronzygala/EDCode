2308b103cf7cb2dd026400883e9c0c0192dd2089
aaronzygala <azygala2016@fau.edu>
2020-04-01 17:38:26 -0400

    bugfix

--- program.py
+++ program.py
@@ -46,9 +46,9 @@ def isOverlapping(range1, range2):
     rSet.intersect(range2)
 
 def isAngleWithinRange(motion, inputRanges):
-    print(motion[0] + " AND " + inputRanges[0])
-    print(motion[1] + " AND " + inputRanges[1])
-    print(motion[2] + " AND " + inputRanges[2])
+    print(str(motion[0]) + " AND " + str(inputRanges[0]))
+    print(str(motion[1]) + " AND " + str(inputRanges[1]))
+    print(str(motion[2]) + " AND " + str(inputRanges[2]))
     if motion[0] in inputRanges[0] and motion[1] in inputRanges[1] and motion[2] in inputRanges[2]:
         return True
     return False
