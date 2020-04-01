a63bf3c3a38e167e761bd162669236a944ff8ae0
aaronzygala <azygala2016@fau.edu>
2020-04-01 17:44:59 -0400

    bugfix

--- motion.py
+++ motion.py
@@ -20,7 +20,6 @@ class Motion:
         for x in input.accelRanges:  # initialize the ranges for each angle
             x = range(int(input.movementVal[i]) * 100 - 5, int(input.movementVal[i]) * 100 + 5)
             i += 1
-        print(str(input.accelRanges))
         return input.movementVal
 
 
